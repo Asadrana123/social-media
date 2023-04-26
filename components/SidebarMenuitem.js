8ba1c1fabf2468bb33a4965e3b945cc94d7cbb30
Your Name <asadrana246@example.com>
2023-04-26 19:12:22 +0530

    create sidebar component

---
+++ components/SidebarMenuitem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function SidebarMenuitem({text,Icon,active}) {
+  return (
+    <div className='hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3 mb-4'>
+      <Icon className='h-7'/>
+      <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
+    </div>
+  )
+}
+
+export default SidebarMenuitem
