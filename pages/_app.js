abcc5d5f4d838904f93f7fec4f5b8afeb815162e
Your Name <asadrana246@example.com>
2023-06-07 00:44:30 +0530

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
 export default function App({ Component, pageProps:{session,...pageProps} }) {
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      window.OneSignal = window.OneSignal || [];
+      OneSignal.push(function () {
+        OneSignal.init({
+          appId: "YOUR_ONE_SIGNAL_ID",
+          notifyButton: {
+            enable: true,
+          },
+        });
+      });
+    }
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []);
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
