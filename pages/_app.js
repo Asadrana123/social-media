cd410981a44edbec7379307a0193ace00e21d628
Your Name <asadrana246@example.com>
2023-06-07 10:54:34 +0530

    fixing

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,6 @@ import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
 export default function App({ Component, pageProps:{session,...pageProps} }) {
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      window.OneSignal = window.OneSignal || [];
-      OneSignal.push(function () {
-        OneSignal.init({
-          appId: "33e21049-b34b-4962-87f9-73029cd74383",
-          notifyButton: {
-            enable: true,
-          },
-        });
-      });
-    }
-    return () => {
-      window.OneSignal = undefined;
-    };
-  }, []);
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
